2a161403259b999efd3a2bcf6a14562f248eaad6
Nicolas V. Solmayor jr <nicvsolgm@gmail.com>
2020-12-25 15:37:20 +0800

    initial commit

---
+++ website/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'home.html', {})
+
+
+def about(request):
+    return render(request, 'about.html', {})
+
+def services(request):
+    return render(request, 'services.html', {})
+
+def work(request):
+    return render(request, 'work.html', {})
+
+def blog(request):
+    return render(request, 'blog.html', {})
+
+def contact(request):
+    return render(request, 'contact.html', {})
