cc91d135dc16024e313ce666b1aa0626b65b0a3f
Nicolas Solmayor <nicvsolgm@gmail.com>
2020-12-25 17:26:27 +0800

    do some changes for deployment

--- dentist/settings.py
+++ dentist/settings.py
@@ -16,9 +16,10 @@ import django_heroku
 import dj_database_url
 from decouple import config
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+#BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -82,7 +83,7 @@ WSGI_APPLICATION = 'dentist.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
