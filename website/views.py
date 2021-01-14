4bbf9bdf662245dc416fc94746b2f7057aae0334
Nicolas Solmayor <nicvsolgm@gmail.com>
2021-01-14 09:30:03 +0800

    merge to master

--- website/views.py
+++ website/views.py
@@ -2,6 +2,7 @@ from .models import Contact
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
 from blog.models import Post
+from .forms import ContactForm
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -9,8 +10,12 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 def home(request):
+    form = ContactForm(request.POST or None)
+    if form.is_valid():
+        form.save()
 
     context = {
+        'form': form,
         'posts': Post.objects.all(),
         'post8': Post.objects.get(pk=8),
         'post9': Post.objects.get(pk=9),
@@ -35,19 +40,33 @@ def work(request):
 
 
 def contact(request):
-    if request.method == 'POST':
-        contact = Contact()
-        name = request.POST['name']
-        phone = request.POST['phone']
-        email = request.POST['email']
-        subject = request.POST['subject'],
-        contact.name = name
-        contact.phone = phone
-        contact.email = email
-        contact.subject = subject
-        contact.save()
+    form = ContactForm(request.POST or None)
+    if form.is_valid():
+        form.save()
 
-    return render(request, 'home.html')
+    context = {
+        'form': form
+    }
+    return render(request,"home.html",context)
+
+
+
+
+
+
+#    if request.method == 'POST':
+#        contact = Contact()
+#        name = request.POST['name']
+#        phone = request.POST['phone']
+#        email = request.POST['email']
+#        subject = request.POST['subject'],
+#        contact.name = name
+#        contact.phone = phone
+#        contact.email = email
+#        contact.subject = subject
+#        contact.save()
+
+#   return render(request, 'home.html')
 
 
 
