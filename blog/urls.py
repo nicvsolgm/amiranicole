14dc40a4f44f91a98b2bad23e8a00f6685eb09e5
Nicolas V. Solmayor Jr <nicvsolgm@gmail.com>
2022-01-04 19:55:24 +0800

    'changes'

--- blog/urls.py
+++ blog/urls.py
@@ -11,7 +11,7 @@ from . import views
 
 urlpatterns = [
     #path('', views.home, name='blog-home'),
-    path('blog/', PostListView.as_view(), name='post-list'),
+    path('', PostListView.as_view(), name='post-list'),
     path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
