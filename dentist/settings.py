119b754868ebef581f4dc9a088549cf74aa69ce3
Nicolas V. Solmayor Jr <nicvsolgm@gmail.com>
2022-01-07 08:52:39 +0800

    'def'

--- dentist/settings.py
+++ dentist/settings.py
@@ -147,11 +147,12 @@ LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'nicvsolgm@gmail.com'
-EMAIL_HOST_PASSWORD = '@Nikylla12'
+#EMAIL_USE_SSL = False
+MAIL_HOST_USER = 'nicvsolgm@gmail.com'
+EMAIL_HOST_PASSWORD = '@Nikylla123'
 
 django_heroku.settings(locals())
--- website/views.py
+++ website/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
 from blog.models import Post
 from .forms import ContactForm
-
+from django.urls import reverse
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.mail import send_mail
+from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
@@ -38,29 +41,40 @@ def work(request):
 
 
 def contact(request):
-    form = ContactForm(request.POST or None)
-    if form.is_valid():
-        form.save()
 
-    context = {
-        'form': form
-    }
-    return render(request, "website/contact.html", context)
+    if request.method == 'POST':
+        name = request.POST.get('Full-name')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+        print(name, subject, message)
 
+        send_mail(
+            subject,
+            message,
+            'nicvsolgm@gmail.com',
+            ['nusolmayor@gmail.com'],
+            fail_silently=False,
+        )
 
-#    if request.method == 'POST':
-#        contact = Contact()
-#        name = request.POST['name']
-#        phone = request.POST['phone']
-#        email = request.POST['email']
-#        subject = request.POST['subject'],
-#        contact.name = name
-#        contact.phone = phone
-#        contact.email = email
-#        contact.subject = subject
-#        contact.save()
+#        return HttpResponseRedirect(reverse('home'))
+#    else:
+#        return HttpResponse('Invalid request')
+    #    data = {
+    #        'name': name,
+    #        'subject': subject,
+    #        'email': email,
+    #        'message': message
+    #    }
+    #    message = ''''
+    #     New message: {}
 
-#   return render(request, 'home.html')
+     #    From: {}
+     #   '''.format(data['message'],data['email'])
+     #   send_mail(data['subject'],data['message'],'',['nicvsolgm@gmail.com'])
+        #return render(request, 'website/contact.html', {'name': name})
+
+   # else:
+    return render(request, 'website/contact.html', {})
 
 def skin(request):
     return render(request, 'website/skin.html', {})
